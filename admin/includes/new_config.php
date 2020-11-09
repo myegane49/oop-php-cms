91e3d1a4119a564d2d8075446c7f617775b79f00
Mohammad Ahmady Yeganeh <myegane49@protonmail.com>
2020-11-09 12:02:50 +0330

    Adds some more new features

---
+++ admin/includes/new_config.php
@@ -0,0 +1,12 @@
+<?php
+// database connection constants
+define('DB_HOST', 'localhost');
+define('DB_USER', 'myself');
+define('DB_PASS', 'qw204080');
+define('DB_NAME', 'gallery_db');
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($connection) {
+  echo "true";
+}
+?>
