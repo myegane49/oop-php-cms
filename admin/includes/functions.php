390427ac4d618a410cf79f81f092c3eeaf2bf0c6
Mohammad Ahmady Yeganeh <myegane49@protonmail.com>
2020-11-14 21:15:08 +0330

    Adds the session class

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -22,4 +22,8 @@ function classAutoLoader($class) {
   }
 }
 spl_autoload_register('classAutoLoader');
+
+function redirect($location) {
+  header("location: {$location}");
+}
 ?>
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -57,6 +57,21 @@ class User {
     $object_properties = get_object_vars($this);
     return array_key_exists($property, $object_properties);
   }
+
+  static function verify_user($username, $password) {
+    global $database;
+    
+    $username = $database->escape_string($username);
+    $password = $database->escape_string($password);
+    $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+
+    $result = self::find_this_query($sql);
+    if (!empty($result)) {
+      $first_item = array_shift($result);
+      return $first_item;
+    }
+    return false;
+  }
 }
 
 ?>
