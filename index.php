6d3827a4e00dcb9e17fa8463ef82d7a28e3325ee
Mohammad Ahmady Yeganeh <myegane49@protonmail.com>
2020-11-23 17:21:08 +0330

    Finishes the project

--- index.php
+++ index.php
@@ -1,30 +1,61 @@
 <?php include("includes/header.php"); ?>
 
+<?php
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+$items_per_page = 2;
+$items_total_count = Photo::count_all();
+
+// $photos = Photo::find_all();
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+$sql = "SELECT * FROM photos LIMIT {$items_per_page} OFFSET {$paginate->skip()}";
+// $sql = "SELECT * FROM photos LIMIT {$paginate->skip()}, {$items_per_page}";
+$photos = Photo::find_this_query($sql);
+?>
+
+<div class="row">
+    <!-- Blog Entries Column -->
+    <div class="col-md-12">
+        <div class="thumbnails row">
+            <?php foreach($photos as $photo): ?>
+                <div class="col-xs-6 col-md-3">
+                    <a href="photo.php?id=<?php echo $photo->id; ?>" class="thumbnail">
+                        <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picture_path(); ?>">
+                    </a>
+                </div>
+            <?php endforeach; ?>
+        </div>
 
         <div class="row">
+            <ul class="pager">
+                <?php if ($paginate->page_total() > 1): ?>
+                    <?php if ($paginate->has_next()): ?>
+                        <li class="next"><a href="index.php?page=<?php echo $paginate->next(); ?>">Next</a></li>
+                    <?php endif; ?>
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
+                    <?php
+                        for ($i = 1; $i <= $paginate->page_total(); $i++) {
+                            if ($i == $paginate->current_page) {
+                                echo "<li class='active'><a href='index.php?page=$i'>$i</a></li>";
+                            } else {
+                                echo "<li><a href='index.php?page=$i'>$i</a></li>";
+                            }
+                        }
+                    ?>
 
-    
-            
-          
-         
-
-            </div>
-
-
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
-
-
-
+                    <?php if ($paginate->has_previous()): ?>
+                        <li class="previous"><a href="index.php?page=<?php echo $paginate->previous(); ?>">Previous</a></li>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </ul>
         </div>
-        <!-- /.row -->
+    </div>
 
-        <?php include("includes/footer.php"); ?>
+    <!-- Blog Sidebar Widgets Column -->
+    <!-- <div class="col-md-4"> -->
+        <?php // include("includes/sidebar.php"); ?>
+    <!-- </div> -->
+
+</div>
+<!-- /.row -->
+
+<?php include("includes/footer.php"); ?>
--- photo.php
+++ photo.php
@@ -1,12 +1,6 @@
+<?php include("includes/header.php"); ?>
+
 <?php
-require_once('admin/includes/functions.php');
-require_once('admin/includes/new_config.php');
-require_once('admin/includes/database.php');
-require_once('admin/includes/db_object.php');
-require_once('admin/includes/user.php');
-require_once('admin/includes/photo.php');
-require_once('admin/includes/session.php');
-require_once('admin/includes/comment.php');
 
 if (empty($_GET["id"])) {
     redirect("index.php");
@@ -32,87 +26,67 @@ if (isset($_POST['submit'])) {
 $comments = Comment::find_the_comments($photo->id);
 ?>
 
-<?php include("includes/header.php"); ?>
+<!-- Blog Post Content Column -->
+<div class="row">
+    <div class="col-lg-12">
+        <!-- Title -->
+        <h1><?php echo $photo->title; ?></h1>
 
-            <!-- Blog Post Content Column -->
-            <div class="col-lg-8">
+        <!-- Author -->
+        <p class="lead">
+            by <a href="#">Chloe Price</a>
+        </p>
+        <hr>
 
-                <!-- Blog Post -->
+        <!-- Date/Time -->
+        <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
+        <hr>
 
-                <!-- Title -->
-                <h1>Blog Post Title</h1>
+        <!-- Preview Image -->
+        <img class="img-responsive" src="admin/<?php echo $photo->picture_path(); ?>" alt="">
+        <hr>
 
-                <!-- Author -->
-                <p class="lead">
-                    by <a href="#">Start Bootstrap</a>
-                </p>
+        <!-- Post Content -->
+        <p class="lead"><?php echo $photo->caption; ?></p>
+        <p><?php echo $photo->description; ?></p>
+        <hr>
 
-                <hr>
-
-                <!-- Date/Time -->
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 24, 2013 at 9:00 PM</p>
-
-                <hr>
-
-                <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-
-                <hr>
-
-                <!-- Post Content -->
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
-
-                <hr>
-
-                <!-- Blog Comments -->
-
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form" method="post">
-                        <div class="form-group">
-                            <label>Author</label>
-                            <input type="text" name="author" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Comment</label>
-                            <textarea name="body" class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-                    </form>
+        <!-- Comments Form -->
+        <div class="well">
+            <h4>Leave a Comment:</h4>
+            <form role="form" method="post">
+                <div class="form-group">
+                    <label>Author</label>
+                    <input type="text" name="author" class="form-control">
                 </div>
+                <div class="form-group">
+                    <label>Comment</label>
+                    <textarea name="body" class="form-control" rows="3"></textarea>
+                </div>
+                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <hr>
 
-                <hr>
+        <!-- Comment -->
+        <?php foreach($comments as $comment): ?>
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading"><?php echo $comment->author; ?></h4>
+                    <?php echo $comment->body; ?>
+                </div>
+            </div>    
+        <?php endforeach; ?>
+        
+    </div>
 
-                <!-- Posted Comments -->
-
-                <!-- Comment -->
-                <?php foreach($comments as $comment): ?>
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading"><?php echo $comment->author; ?></h4>
-                            <?php echo $comment->body; ?>
-                        </div>
-                    </div>    
-                <?php endforeach; ?>
-                
-
-            </div>
-
-             <!-- Blog Sidebar Widgets Column -->
-             <div class="col-md-4">
-
-            
-<?php include("includes/sidebar.php"); ?>
-
-
+<!-- Blog Sidebar Widgets Column -->
+<!-- <div class="col-md-4">       -->
+    <?php // include("includes/sidebar.php"); ?>
+<!-- </div> -->
 
 </div>
 <!-- /.row -->
